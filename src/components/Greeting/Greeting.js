5c7fce4e5e3248f5f32a90a7450efe4e1292d090
tccSolutions <83311084+tccSolutions@users.noreply.github.com>
2021-04-27 22:44:35 -0400

    css update

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -6,10 +6,18 @@ class Greeting extends React.Component{
     render(){
         return(
             <div className='greeting'>
+                <div>
                 <div className='dcs'>
                     <h1 style = {{marginBottom:'0'}}>Tim's Custom Computer Solutions</h1>                   
                     <img alt= 'underline' src={underline} style={{lineHeight:'0'}} />              
                 </div>
+                <div className='dcs_2'> 
+                                    Thank you for visiting TCCS.tech. We offer a 
+                                    variety of services ranging from pc repair to web development.
+                                    Please use<span className='link '> Contact Us</span> link if you would like a quote or have a 
+                                    question.
+                </div>
+            </div>
             </div>
 
             
--- src/components/GridMenu/GridMenu.js
+++ src/components/GridMenu/GridMenu.js
@@ -29,17 +29,13 @@ class GridMenu extends React.Component{
     render(){
        const {onRouteChange} = this.props;
         return(
-            <div className='grid-menu'>
-                
-                <div>
+            <div className='grid-menu center'>
+               
+                <div className=''>
                     {/* Welcome Button */}
                     <div className='cell-container'>
                         <div id='welcome'  className='cell'>
-                            <div id='welcome-front' className='inner-cell' onClick={()=>{
-                                this.addClass('welcome', 'grow')
-                                this.toggleView('welcome-front')
-                                this.toggleView('welcome-back') 
-                                }} >
+                            <div id='welcome-front' className='inner-cell' onClick={()=>{onRouteChange("home")}} >
                                 <svg   width="5em" height="5em" viewBox="0 0 16 16" className="bi bi-house-door" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fillRule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z"/>
                                     <path fillRule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
@@ -122,10 +118,10 @@ class GridMenu extends React.Component{
                         </div>
                     </div>
                 </div>
-                
+                </div>
                
 
-            </div>
+            
         
         );
 
