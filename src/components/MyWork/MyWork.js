ca4d71d6af6bda27f8651ad602c904e9fe79897b
tccSolutions <83311084+tccSolutions@users.noreply.github.com>
2021-04-30 09:38:53 -0400

    workpage update

--- src/components/MyWork/MyWork.js
+++ src/components/MyWork/MyWork.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ac from '../../data/images/MDCS.jpg'
+import Python from '../PythonProjects/Python'
 
 class MyWork extends React.Component {
     render() {
@@ -9,29 +9,14 @@ class MyWork extends React.Component {
                 <div className='fs-1 mt-5 fw-bold d-flex justify-content-center'>
                     PC Repair and Builds
                     </div>
-                <div className='fs-1 mt-5 fw-bold justify-content-center'>
-                    <h2>Python Projects</h2>
-                    <div className='d-flex justify-content-center mt-5' >
-                    <div className='w-50 d-flex justify-content-center'>
-                        <div className='justify-content-center'>
-                        <img src={ac} alt='' width='300px'/>
-                        </div>
-                        <div className='w-50 d-flex justify-content-center mt-5'>
-                            <h4>This program was designed to help automate the paperwork involed in classifying animals. The user simply inputs the required and the program inserts
-                            the information into the required fields.
-                        </h4>
-                        </div>
-                        </div>
-                       
-                        
-                    </div>
-                    </div>
+                    <Python/>
+               
                 <div className='fs-1 mt-5 fw-bold justify-content-center'>
                     <h2>Web Projects</h2>
                     <div className="d-flex justify-content-center">
                         <h4 className='w-75 mt-3'>
                             The team at TCCS.TECH has developed many different websites for a variety of different needs.
-                            The website you are currently viewing was developed soley by the team TCCS.TECH. You can click the links below to visit them.
+                            The website you are currently viewing was developed by the team TCCS.TECH using REACT. You can click the links below to visit them.
                             If you have any questions or request please use the
                             <span className='pointer' onClick={() => onRouteChange("contactUs")}><strong > Contact Us</strong></span> link.
                         </h4>
---
+++ src/components/PythonProjects/Python.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import ac from '../../data/images/MDCS.jpg'
+import letter from '../../data/images/letter.jpg'
+import nhl from '../../data/images/nhlstandings.jpg'
+
+class Python extends React.Component {
+    render() {
+        return (
+            <div className='d-flex justify-content-center mt-5'>               
+                <div id="carouselExampleControls" class="carousel slide w-75 " data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                    <div className='justify-content-center'>
+                        <h3><a style={{color:'white'}} href="https://www.python.org/about/apps/">Python Projects</a></h3>
+                        <p>Python is a versatile language that is great for web scraping and automation.<br></br> The programs displayed below were developed
+                            by the TCCS.TECH team using python.
+                        </p>
+                    </div>
+                        <div class="carousel-item active justify-content-center mt-3">
+                            {/* photo 1 */}
+                            <div className='justify-content-center'>
+                                <img src={ac} alt='' width='300px' />
+                                <p>This program was designed for an Animal Services Agency to automate the paper work required for certain animals.</p>
+                            </div>
+                        </div>
+                         {/* photo 2 */}
+                        <div class="carousel-item ">                            
+                                <div className='d-flex flex-row justify-content-center'>
+                                <img src={letter} alt='' height='300px' />
+                                </div>
+                                <div className='d-flex flex-row justify-content-center'>
+                                <p className='w-50'>This program was designed for a Code Enforcement Agency. 
+                                    It uses <a className='light pointer' style={{color:'white'}} href='https://www.parsehub.com/blog/what-is-web-scraping/'>Web Scraping</a> 
+                                     to collect property owner information. The program then formats a letter that is then mailed to the property owner.</p>
+                                </div>                   
+                        </div>
+                        {/*photo 3 */}
+                        <div class="carousel-item ">
+                            <div className='d-flex flex-row justify-content-center'>
+                                <img src={nhl} alt='' height='300px' />
+                            </div>
+                            <div className='d-flex flex-row justify-content-center'>
+                                <p className='flex-row w-50 '>This is a personal program that uses Web Scraping to get the current NHL League Standings.<br></br>
+                                                                        I might be a little bit of a hockey fan.</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Python;
