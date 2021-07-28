b373481e604eff38db347dfa88fb20bb90753c61
tccSolutions <tim@tccs.tech>
2021-07-28 13:33:29 -0400

    navbar update

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,7 +7,7 @@ class Nav extends React.Component {
         return (
          <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#">Tccs.Tech</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
